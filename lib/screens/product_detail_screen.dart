330594af5daa8427d1a554c05a9881bfd11441fa
nporoykov <nporoykov@gmail.com>
2021-05-30 00:07:28 +0300

    start

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+import 'package:provider/provider.dart';
+
+import '../providers/products.dart';
+
+class ProductDetailScreen extends StatelessWidget {
+  static const routeName = '/product-detail';
+  // final String title;
+  // final double price;
+
+  // ProductDetailScreen(this.title, this.price);
+
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
+    //.....
+    return Scaffold(
+        appBar: AppBar(
+      title: Text(loadedProduct.title),
+    ));
+  }
+}
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/product.dart';
+
+import '../screens/product_detail_screen.dart';
+
+class ProductItem extends StatelessWidget {
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+
+  // ProductItem(this.id, this.title, this.imageUrl);
+
+  @override
+  Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(10),
+      child: GridTile(
+        child: GestureDetector(
+          onTap: () {
+            Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                arguments: product.id);
+          },
+          child: Image.network(
+            product.imageUrl,
+            fit: BoxFit.cover,
+          ),
+        ),
+        footer: GridTileBar(
+            leading: IconButton(
+                icon: Icon(Icons.shopping_cart),
+                onPressed: null,
+                color: Theme.of(context).accentColor),
+            backgroundColor: Colors.black87,
+            title: Text(
+              product.title,
+              textAlign: TextAlign.center,
+            ),
+            trailing: IconButton(
+                icon: Icon(product.isFavorite
+                    ? Icons.favorite
+                    : Icons.favorite_border),
+                onPressed: () => product.toggleFavoriteStatus(),
+                color: Theme.of(context).accentColor)),
+      ),
+    );
+  }
+}
